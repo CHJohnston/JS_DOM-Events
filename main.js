b5b4d87a68ab4f6cfd3e0ccc4c0e473c421590e4
CHJohnston <cdh.johnston@gmail.com>
2022-06-17 14:35:26 -0500

    Completed Exercise

--- main.js
+++ main.js
@@ -3,41 +3,90 @@ console.log("Script attached");
 
 // Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
 
-// Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
+let node1 = document.getElementById("node1");
+console.log(node1);
+node1.textContent = 'I used the getElementById("node1") method to access this';
 
-// Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
+// Select Node #2 and change the text to: "I used the getElementsByClassName("node2") method to access this" */
+//let node2 = document.getElementsByClassName("node2");
+let node2 = document.getElementsByClassName("node2");
+console.log(node2);
+node2[0].textContent =
+  'I used the getElementsByClassName("node2") method to access this';
+
+// Select ALL the h3 tags and change the text to: "I used the getElementsByTagName("h3") method to access all of these" */
+let h3 = document.getElementsByTagName("h3");
+console.log(h3);
+for (value of h3) {
+  value.textContent =
+    'I used the getElementsByTagName("h3") method to access all of these';
+}
 
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using the createElement() method"
 
+let p = document.createElement("p");
+p.textContent = "This node was created using the createElement() method";
+
 // TODO: Append the created node to the parent node using the element.appendChild() method
+let parent2 = document.querySelector("#parent");
+parent2.appendChild(p);
 
 // TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
-
 // BONUS: Add a link href to the <a>
+let link = document.createElement("a");
+link.textContent = "Google Search";
+link.href = "https://www.google.com/";
 
 // TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+parent2.insertBefore(link, p);
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
-
 // TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
 
+let child = document.createElement("p");
+child.textContent = "New Child Node";
+let parent3 = document.querySelector("#exercise-container3");
+let replaceChild = document.querySelector("#N1");
+parent3.replaceChild(child, replaceChild);
+
 // TODO: Remove the "New Child Node"
+setTimeout(() => {
+  parent3.removeChild(child);
+}, 5000);
 
 /*----------- Exercise #4: ANIMATIONS ----------- */
 // Use the following array of values to generate a list on the DOM
 
-let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish", "grapes", "honey", "ice bag", "juice (any kind)" ];
-
+let list = [
+  "apples",
+  "bananas",
+  "carrots",
+  "dragon fruit",
+  "eggplant",
+  "fish",
+  "grapes",
+  "honey",
+  "ice bag",
+  "juice (any kind)",
+];
 
 // TODO: Create an unordered list element
+let ul = document.createElement("ul");
 
 // TODO: Iterate over the array values, and create a list item element for each
-
 // TODO: Append the new list items to the unordered list element
 
-// TODO: Append the unordered list to the `div#container` under exercise 4 
+for (item of list) {
+  let li = document.createElement("li");
+  li.textContent = item;
+  ul.appendChild(li);
+}
+
+// TODO: Append the unordered list to the `div#container` under exercise 4
+let container4 = document.querySelector("#container");
+container4.appendChild(ul);
 
 /*----------- Exercise #5: DOM EVENTS --------------*/
 
@@ -45,3 +94,46 @@ let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish",
 // -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
 // This div should be a 'modal' that covers the main content on the screen
 // BONUS: The modal popup should be able to be closed. Refactor for this functionality
+
+//Define Function to close modal on ESC key
+function handleEsc(event) {
+  if (event.key == "Escape") {
+    document.body.removeChild(document.querySelector("#modal"));
+  }
+}
+
+function show(event) {
+  //Call Function to close modal on ESC key
+  window.addEventListener("keyup", handleEsc);
+
+  let modalContainer = document.createElement("div");
+  let modalBody = document.createElement("div");
+  let title = document.createElement("h2");
+  let content = document.createElement("p");
+  let closeButton = document.createElement("button");
+
+  title.textContent = "Action Required";
+  content.textContent = "You need to ...";
+  closeButton.textContent = "Close";
+  closeButton.addEventListener("click", (event) => {
+    document.body.removeChild(modalContainer);
+    window.removeEventListener("keyup", handleEsc);
+  });
+
+  modalContainer.id = "modal";
+  modalBody.classList.add("modal-card");
+
+  modalContainer.addEventListener("click", (event) => {
+    if (event.target == modalContainer) {
+      document.body.removeChild(modalContainer);
+      window.removeEventListener("keyup", handleEsc);
+    }
+  });
+
+  modalBody.append(title, content, closeButton);
+  modalContainer.appendChild(modalBody);
+  document.body.appendChild(modalContainer);
+}
+
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", show);
